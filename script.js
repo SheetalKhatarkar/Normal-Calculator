8e00bcc5ba55b284a3589946757453ed19a5f909
SheetalKhatarkar <168743576+SheetalKhatarkar@users.noreply.github.com>
2024-05-15 19:14:55 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 /* function for themes*/
 
-/*const theme1=document.getElementById("one")
-const theme2=document.getElementById("two")
-const theme3=document.getElementById("three")
-const containerTheme=document.querySelector(".container") 
-
 theme2.addEventListener("click",function(){
 containerTheme.classList.add("light-teme")
 containerTheme.classList.remove("contrast-teme")
